5333c0fc9202b02bba6952e958a7a23fd8794409
insiyazain <insiyazain123@gmail.com>
2024-11-01 17:15:38 +0500

    navbar

--- mywebsite/Home/views.py
+++ mywebsite/Home/views.py
@@ -3,4 +3,16 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("This is homepage")
+    context={
+        'variable':"this is sent"
+    }
+    return render(request,"index.html", context)
+
+def about(request):
+    return render(request, "about.html")
+
+def services(request):
+    return render(request,"services.html")
+
+def contact(request):
+    return render(request,"contact.html")
